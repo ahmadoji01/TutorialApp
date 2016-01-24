a835f42fc485ce28a0bc0fadd365ff871e88e726
uzzybotak <knightindustriesfans@yahoo.com>
2016-01-24 15:05:55 +0700

    Login Registration Step 1
    PHP preparation and App configuration

---
+++ app/src/main/java/com/tutorialapp/tutorialapp/AppConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Created by michaelknight123 on 1/24/2016.
+ */
+
+package com.tutorialapp.tutorialapp;
+
+public class AppConfig
+{
+        // Server user login url
+        public static String URL_LOGIN = "http://192.168.0.102:8888/android_login_api/login.php";
+
+        // Server user register url
+        public static String URL_REGISTER = "http://192.168.0.102:8888/android_login_api/register.php";
+}
--- app/src/main/java/com/tutorialapp/tutorialapp/MainActivity.java
+++ app/src/main/java/com/tutorialapp/tutorialapp/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.android.volley.toolbox.ImageLoader.ImageCache;
 
 public class MainActivity extends AppCompatActivity {
 
